d0b9a87369d13c649e92e566fc08bf029e4bf246
Muhammad Hafiz Bin Rodzli <hafizrodzli25@gmail.com>
2026-06-02 21:46:18 +0800

    Refactor Index and Show pages to use MainLayout component for consistent layout structure

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,16 +1,15 @@
-
 <template>
-    <div>Index</div>
-    <div>
-        The message is: {{ message }}
-    </div>
-    <Link href="/hello">Show Page</Link>
+    <MainLayout>
+        <div>Index</div>
+        <Link href="/hello">Show Page</Link>
+        <div>The message is: {{ message }}</div>
+    </MainLayout>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import { Link } from "@inertiajs/vue3";
+import MainLayout from "../../Layouts/MainLayout.vue";
 defineProps({
-    message: String
+    message: String,
 });
-
 </script>
