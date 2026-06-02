871d443dce98f4bf1069e3026368abed953291b5
Muhammad Hafiz Bin Rodzli <hafizrodzli25@gmail.com>
2026-06-02 21:30:39 +0800

    Add message prop to Index page and update controller to pass data

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,10 +1,16 @@
 
 <template>
     <div>Index</div>
+    <div>
+        The message is: {{ message }}
+    </div>
     <Link href="/hello">Show Page</Link>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3';
+defineProps({
+    message: String
+});
 
 </script>
