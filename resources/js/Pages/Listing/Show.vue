7fe9905421db9dbef535456e06d793a7108d4fcd
Muhammad Hafiz Bin Rodzli <hafizrodzli25@gmail.com>
2026-06-10 14:51:00 +0800

    feat: create ListingAddress component and integrate it into Index and Show pages

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,7 +1,13 @@
-<script setup>
-
-</script>
-
 <template>
-
+    <div>
+        <ListingAddress :listing="listing" />
+    </div>
 </template>
+
+<script setup>
+import ListingAddress from "@/Components/ListingAddress.vue";
+
+defineProps({
+    listing: Object,
+});
+</script>
