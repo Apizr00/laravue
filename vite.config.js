50e071968bf1edeff2d0d8463bbecc96257aec20
Muhammad Hafiz Bin Rodzli <hafizrodzli25@gmail.com>
2026-06-13 16:07:55 +0800

    feat: add Ziggy for route management; update routes in Listing components and Blade template

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
-import vue from '@vitejs/plugin-vue';
+import path from "path";
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import { bunny } from "laravel-vite-plugin/fonts";
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
             fonts: [
-                bunny('Instrument Sans', {
+                bunny("Instrument Sans", {
                     weights: [400, 500, 600],
                 }),
             ],
@@ -24,7 +25,7 @@ export default defineConfig({
                     // to instead re-write asset URLs to point to the Vite
                     // server instead.
                     base: null,
- 
+
                     // The Vue plugin will parse absolute URLs and treat them
                     // as absolute paths to files on disk. Setting this to
                     // `false` will leave absolute URLs un-touched so they can
@@ -35,9 +36,14 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            ziggy: path.resolve("vendor/tightenco/ziggy/dist/index.esm.js"),
+        },
+    },
     server: {
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
         },
     },
 });
