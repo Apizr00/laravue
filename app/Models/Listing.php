37b2d45662afb1d0d688f1bd5d4c5125be72e817
Muhammad Hafiz Bin Rodzli <hafizrodzli25@gmail.com>
2026-06-13 15:41:57 +0800

    feat: implement full CRUD functionality for listings; enhance validation and error handling; add edit form

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -5,6 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $beds
+ * @property int $baths
+ * @property int $area
+ * @property string $city
+ * @property string $code
+ * @property string $street
+ * @property string $street_nr
+ * @property int $price
+ * @method static \Database\Factories\ListingFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Listing newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Listing newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Listing query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Listing whereArea($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Listing whereBaths($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Listing whereBeds($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Listing whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Listing whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Listing whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Listing whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Listing wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Listing whereStreet($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Listing whereStreetNr($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Listing whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Listing extends Model
 {
     use HasFactory;
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,12 +1,8 @@
 <template>
-    <Link href="/">Main Page</Link>&nbsp;
-    <Link href="/hello">Show Page</Link>
-    <!-- <div>The page with time {{ timer }}</div> -->
+    <Link href="/listing">Listings</Link>&nbsp;
+    <Link href="/listing/create">New Listing</Link>
     <div v-if="flashSuccess" class="success">
         {{ flashSuccess }}
-    </div>
-    <div v-if="flashError" class="error">
-        {{ flashError }}
     </div>
 
     <slot>Default</slot>
@@ -15,12 +11,6 @@
 <script setup>
 import { computed } from "vue";
 import { Link, usePage } from "@inertiajs/vue3";
-// import { ref } from "vue";
-
-// const timer = ref(0);
-// setInterval(() => {
-//     timer.value++;
-// }, 1000);
 
 const page = usePage();
 const flashSuccess = computed(() => page.props.flash.success);
