b084b38a430e3e087e629026a2956b6732c55d86
Muhammad Hafiz Bin Rodzli <hafizrodzli25@gmail.com>
2026-06-14 22:45:04 +0800

    feat: implement authentication logic in AuthController and update IndexController message

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,26 +2,24 @@
 
 namespace Database\Seeders;
 
-// use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
         \App\Models\Listing::factory(20)->create();
     }
 }
