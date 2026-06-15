547456f4bf678245284ed4d0f26ff192451dc471
Muhammad Hafiz Bin Rodzli <hafizrodzli25@gmail.com>
2026-06-15 20:30:49 +0800

    feat: add user account creation page and update routing for user account management

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserAccountController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -18,3 +19,6 @@ Route::post('login', [AuthController::class, 'store'])
     ->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])
     ->name('logout');
+
+Route::resource('user-account', UserAccountController::class)
+    ->only(['create']);
