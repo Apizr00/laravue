fcdf3f62a09531590cffa33d5d9588ad006fab3a
Muhammad Hafiz Bin Rodzli <hafizrodzli25@gmail.com>
2026-06-21 14:02:58 +0800

    feat: refactor Listing model to use attribute scopes for filtering and sorting in index method

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Attributes\Scope;
 
 /**
  * @property int $id
@@ -38,29 +39,55 @@ use Illuminate\Database\Eloquent\Builder;
  */
 class Listing extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $fillable = [
-        'beds',
-        'baths',
-        'area',
-        'city',
-        'code',
-        'street',
-        'street_nr',
-        'price',
-    ];
+  protected $fillable = [
+    'beds',
+    'baths',
+    'area',
+    'city',
+    'code',
+    'street',
+    'street_nr',
+    'price',
+  ];
 
-    public function owner(): BelongsTo
-    {
-        return $this->belongsTo(
-            \App\Models\User::class,
-            'by_user_id'
-        );
-    }
+  public function owner(): BelongsTo
+  {
+    return $this->belongsTo(
+      \App\Models\User::class,
+      'by_user_id'
+    );
+  }
 
-    public function scopeMostRecent(Builder $query): Builder
-    {
-        return $query->orderByDesc('created_at');
-    }
+  #[Scope]
+  protected function mostRecent(Builder $query): void
+  {
+    $query->orderByDesc('created_at');
+  }
+
+  #[Scope]
+  protected function filter(Builder $query, array $filters): void
+  {
+    $query
+      ->when(
+        $filters['priceFrom'] ?? false,
+        fn($query, $value) => $query->where('price', '>=', $value)
+      )->when(
+        $filters['priceTo'] ?? false,
+        fn($query, $value) => $query->where('price', '<=', $value)
+      )->when(
+        $filters['beds'] ?? false,
+        fn($query, $value) => $query->where('beds', (int)$value < 6 ? '=' : '>=', $value)
+      )->when(
+        $filters['baths'] ?? false,
+        fn($query, $value) => $query->where('baths', (int)$value < 6 ? '=' : '>=', $value)
+      )->when(
+        $filters['areaFrom'] ?? false,
+        fn($query, $value) => $query->where('area', '>=', $value)
+      )->when(
+        $filters['areaTo'] ?? false,
+        fn($query, $value) => $query->where('area', '<=', $value)
+      );
+  }
 }
